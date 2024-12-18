76f675ac42a197502fb4b343094a3139b19dee23
Ahmed Bakry <ahmed.elbakry.ab@gmail.com>
2024-12-18 23:56:08 +0200

    Program.cs

--- Assignment Session4/Program.cs
+++ Assignment Session4/Program.cs
@@ -386,7 +386,6 @@ namespace Assignment_Session4
                     int largest = int.MinValue;
                     int secondLargest = int.MinValue;
             
-                    // Find the largest and second largest elements
                     for (int i = 0; i < array.Length; i++)
                     {
                         if (array[i] > largest)
